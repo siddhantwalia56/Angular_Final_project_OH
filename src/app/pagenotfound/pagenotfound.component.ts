1033a629d7955f6ad68372142a560cfd90e5f276
SiddhantOnceHub <siddhant.walia@oncehub.com>
2023-04-09 15:17:46 +0530

    created page not found component

--- src/app/pagenotfound/pagenotfound.component.ts
+++ src/app/pagenotfound/pagenotfound.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pagenotfound',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagenotfound.component.css']
 })
 export class PagenotfoundComponent {
-
+constructor(private router: Router){}
+signin(){
+this.router.navigateByUrl('')
+}
 }
